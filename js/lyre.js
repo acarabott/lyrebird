f250203f0fecff964bed6396d44a698ce9489c9d
Arthur Carabott <arthurc@gmail.com>
2013-01-19 15:18:27 +0100

    printing waveform

--- js/lyre.js
+++ js/lyre.js
@@ -1,15 +1,19 @@
 /*jslint newcap: true, plusplus: true, passfail: true, browser: true, devel: true, indent: 4, maxlen: 100 */
-/*global webkitAudioContext: false, createJRemixer: false, jQuery: false, $: false */
+/*global webkitAudioContext: false, createJRemixer: false, jQuery: false, $: false,
+		Waveform: false */
 var apiKey = 'YLTCU72SODVIC00NB';
 var trackID = 'TRFINBO13C52D7A637';
 var trackURL = 'audio/something.mp3';
+var trackWave = 'data/somethingWave.json';
 
-var remixer, player, track, remixed;
+var remixer, player, track, remixed,
+	waveformRequest, waveform, waveformData;
 
 (function () {
 	'use strict';
+	var context;
 
-	var context = new webkitAudioContext();
+	context = new webkitAudioContext();
 
 	remixer = createJRemixer(context, $, apiKey);
 	player = remixer.getPlayer();
@@ -22,8 +26,27 @@ var remixer, player, track, remixed;
 			$('#info').text(percent + "% of the track loaded, remix time");
 		}
 
-		if (track.status === 'ok') {
-			console.log(track.analysis);
-		}
+		// if (track.status === 'ok') {
+		// 	console.log(track.analysis);
+		// }
 	});
+
+	$.getJSON(trackWave, function (data) {
+		waveformData = data.mid;
+		waveform = new Waveform({
+			container: document.getElementById('waveform'),
+			data: waveformData.slice(0)
+		});
+	});
+
+	// waveformRequest = new XMLHttpRequest();
+	// waveformRequest.open('GET', trackWave, true);
+	// waveformRequest.onload = function () {
+	// 	waveformData = $.parseJSON(waveformRequest.response).mid;
+	// 	waveform = new Waveform({
+	// 		container: document.getElementById('waveform'),
+	// 		data: waveformData.slice(0)
+	// 	});
+	// };
+	// waveformRequest.send();
 }());
