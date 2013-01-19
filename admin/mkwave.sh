93f0a2759bbfa56ed26ee0e761c7327d3150c74b
Arthur Carabott <arthurc@gmail.com>
2013-01-19 15:14:21 +0100

    fixed wave script

--- admin/mkwave.sh
+++ admin/mkwave.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 # make wav
-ffmpeg -i $1 -vn -acodec pcm_s16le "${1%.*}".wav;
+# ffmpeg -i $1 -vn -acodec pcm_s16le "${1%.*}".wav;
 
 # make waveforn json
 b="$(basename $1)";
-wav2json --channels mid -o echo "${b%.*}wave".json "${1%.*}".wav;
-
+wav2json --channels mid -o data/"${b%.*}Wave".json "${1%.*}".wav;
